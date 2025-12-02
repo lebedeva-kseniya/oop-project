a4120704c2bc98a005c6bef7d384bf2e49380cf8
Vadim-000 <Vadim624350@yandex.ru>
2025-12-02 09:52:08 +0500

    Create BotTest.java
    добавлен тест для bot

---
+++ test/BotTest.java
@@ -0,0 +1,12 @@
+import main.Bot;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BotTest {
+
+    @Test
+    public void testConstructor() {
+        Bot bot = new Bot();
+        assertNotNull(bot);
+    }
+}
