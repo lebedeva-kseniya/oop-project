7b986f45bcf04e577ea67026abd0e92e000c50f9
Vadim-000 <Vadim624350@yandex.ru>
2025-12-12 16:16:39 +0500

    переписан метод нахождения слова в интернете
    вместо 2 методов стал 1

--- main/Storage.java
+++ main/Storage.java
@@ -7,26 +7,20 @@ public class Storage extends StorageOfWords{
         Random random=new Random();
         return WORDS[random.nextInt(countWords)];
     }
-    public  String getWord6(){
-        String url="https://sanstv.ru/randomWord/lang-ru/strong-2/count-1/word-%3F%3F%3F%3F%3F%3F";
+    public  String getWordFromWeb(int length){
+        String url;
+        if (length==6)
+            url="https://sanstv.ru/randomWord/lang-ru/strong-2/count-1/word-%3F%3F%3F%3F%3F%3F";
+        else
+            url="https://sanstv.ru/randomWord/lang-ru/strong-2/count-1/word-%3F%3F%3F%3F%3F%3F%3F";
         Document doc;
         try {
             doc = Jsoup.connect(url).get();
             int index=doc.toString().indexOf("target=\"_blank \"")+17;
-            return doc.toString().substring(index,index+6).toUpperCase();
-        } catch (Exception e){
-            return null;
-        }
-    }
-    public  String getWord7(){
-        String url="https://sanstv.ru/randomWord/lang-ru/strong-2/count-1/word-%3F%3F%3F%3F%3F%3F%3F";
-        Document doc;
-        try {
-            doc = Jsoup.connect(url).get();
-            int index=doc.toString().indexOf("target=\"_blank \"")+17;
-            return doc.toString().substring(index,index+7).toUpperCase();
+            return doc.toString().substring(index,index+length).toUpperCase();
         } catch (Exception e){
             return null;
         }
     }
 }
+
